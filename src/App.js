2172877d59b61b2f39b7f41ee5e72155d9ad9a0a
bhaskar <bhaskar25@hotmail.com>
2020-05-02 01:48:32 +0530

    displaying fetched data in table

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import "./styles.css"
 
 class App extends React.Component {
     state = {
-        country: "india",
-        data: null
+        country: "",
+        queryData: [],
+        enableButton: false,
     };
 
     handleOnChange = event => {
         this.setState ({
-            country: "india"
+            country: event.target.value,
+            enableButton: event.target.value !== ""
         });
     }
 
@@ -17,9 +19,51 @@ class App extends React.Component {
         this.makeApiCall()
     };
 
+    makeApiCall = () => {
+        let apiUrl = `https://api.covid19api.com/live/country/${this.state.country}`;
+        fetch(apiUrl)
+            .then(response => {
+                return response.json()
+            })
+            .then(jsonData => {
+                console.log(jsonData)
+                this.setState({queryData: jsonData})
+            })
+    }
+
     render(){
         return (
-            <h1>COVID-TRACKER {new Date().getFullYear()} </h1>
+            <div>
+                <h1>COVID-19 TRACKER APP</h1>
+                <input
+                    name="text"
+                    placeholder="Enter Country Name"
+                    onChange={event=>this.handleOnChange(event)}
+                />
+                <br/><br/>
+                <button onClick={this.handleSearch} disabled={!this.state.enableButton}>Load</button>
+                <br/><br/>
+                {
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Confirmed</th>
+                                <th>Deaths</th>
+                                <th>Recovered</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.queryData.map((day) => (
+                                <tr>
+                                    <td>{day.Confirmed}</td>
+                                    <td>{day.Deaths}</td>
+                                    <td>{day.Recovered}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                }
+            </div>
         )
     }
 }
